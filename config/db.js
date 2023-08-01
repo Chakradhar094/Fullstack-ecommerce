a4a4f04fc8ef2177da1c849420ea3ce12544ec0d
Chakradhar094 <104595679+Chakradhar094@users.noreply.github.com>
2023-08-01 20:24:40 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
-mongoose.connect(config.DB,{ useNewUrlParser: true }));
+import dotenv from "dotenv"
+dotenv.config();
 const connectDB=async()=>{
     try {
         const conn=await mongoose.connect(process.env.MONGO_URL);
